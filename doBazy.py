a14a2d7f9939d9c1f648b47a205e04b8346929d9
Arrato <jacek.niewiadomski@gmail.com>
2020-02-13 19:40:30 +0100

    9:38 sto tysięcy rekordów

--- doBazy.py
+++ doBazy.py
@@ -11,22 +11,23 @@ teraz  = datetime.datetime.now()
 con.execute("""CREATE TABLE IF NOT EXISTS fejkowa
                 (id  INTEGER PRIMARY KEY AUTOINCREMENT, data text, lat text, lon text, kompas INT, napiecie REAL, pochylenie INT , przechylenie INT)""")
 print('Baza utworzona')
-for _ in range(10):
-    rekord = 1
+rekord = 1
+for _ in range(100000):
+    
     data = str(fake.date_time_this_year(before_now=True, after_now=False, tzinfo=None))
     lat = str(fake.latitude())
     lon = str(fake.longitude())
     kompas = fake.random_int(min=0, max=360, step=1)
     napiecie  = (fake.random_int(min=0, max=120, step=1) / 10) #dziele zeby byly po przecinku
-    przechylenie  = fake.random_int(min=0, max=90, step=1)  #dziele zeby byly po przecinku
-    pochylenie  = fake.random_int(min=0, max=90, step=1)  #dziele zeby byly po przecinku
+    przechylenie  = fake.random_int(min=0, max=90, step=1)  #te sa z dokladnoscia do stopnia
+    pochylenie  = fake.random_int(min=0, max=90, step=1)  #z dokladnoscia do stopnia
     print(data, lat, lon, kompas, napiecie, przechylenie, pochylenie)
     # upload do bazy danych każdego rekordu
     parametry = (data,lat,lon,kompas,napiecie,pochylenie,przechylenie)
     con.execute("INSERT INTO fejkowa (data ,lat, lon, kompas,napiecie,pochylenie,przechylenie ) VALUES (?,?,?,?,?,?,?)",parametry)
     con.commit()
     print(rekord)
-    rekord = rekord+1
+    rekord = (rekord+1)
 # odczyt danych z bazy 
 print('--------------------ODCZYT Z BAZY SQLITE---------------')
 for row in con.execute("SELECT * from fejkowa"):
