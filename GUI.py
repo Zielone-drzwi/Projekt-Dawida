10919c38575033c1a4d32f596269bb3a4ffab5ff
Arrato <jacek.niewiadomski@gmail.com>
2020-02-18 20:36:33 +0100

    nowe podejscie do tematu, tym razem API

--- GUI.py
+++ GUI.py
@@ -1,7 +1,15 @@
 from PyQt5 import  QtWidgets, uic
+from PyQt5.QtWidgets import QTableWidget,QTableWidgetItem
+
+def Convert():
+    dlg.lineEdit_2.setText(str(float(dlg.lineEdit.text())* 1.23))
 
 app  = QtWidgets.QApplication([])
 dlg = uic.loadUi("test.ui")
 
+dlg.pushButton.clicked.connect(Convert)
+
+dlg.tableWidget.setRowCount(4)
+dlg.tableWidget.setColumnCount(2)
 dlg.show()
 app.exec()
